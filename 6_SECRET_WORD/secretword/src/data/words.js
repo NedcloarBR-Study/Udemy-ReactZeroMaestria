f1fc7598437f154d5e3001e436a7be59c6b4662b
NedcloarBR <nedcloar1@hotmail.com>
2024-11-30 17:44:17 -0300

    feat(secretword): init project

---
+++ 6_SECRET_WORD/secretword/src/data/words.js
@@ -0,0 +1,8 @@
+export const wordsList = {
+  carro: ["Motor", "Porta", "Capô", "Volante", "Antena"],
+  fruta: ["Banana", "Maçã", "Pera", "Mamão", "Laranja"],
+  corpo: ["Braço", "Perna", "Cérebro", "Pescoço", "Olhos"],
+  computador: ["Mouse", "Teclado", "Monitor", "Gabinete"],
+  programação: ["Linguagem", "Framework", "JavaScript", "React"],
+  alimento: ["Arroz", "Feijão", "Carne", "Leite", "Ovo"],
+};
