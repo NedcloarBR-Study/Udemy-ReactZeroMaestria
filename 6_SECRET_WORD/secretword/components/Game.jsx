f2bc4b1e9617c389ab8501ff51897882b3a414da
NedcloarBR <nedcloar1@hotmail.com>
2024-11-30 19:12:28 -0300

    feat: add game router

--- 6_SECRET_WORD/secretword/components/Game.jsx
+++ 6_SECRET_WORD/secretword/components/Game.jsx
@@ -1,9 +1,10 @@
 import "./Game.css";
 
-const Game = () => {
+const Game = ({verifyLetter}) => {
   return (
     <div>
-      Game
+      <h1>Game</h1>
+      <button onClick={verifyLetter}>Finalizar jogo</button>
     </div>
   )
 }
--- 6_SECRET_WORD/secretword/src/App.jsx
+++ 6_SECRET_WORD/secretword/src/App.jsx
@@ -1,6 +1,6 @@
 import StartScreen from '../components/StartScreen'
 import { useCallback, useEffect, useState } from 'react'
-import { wordsList } from '../data/words'
+import { wordsList } from './data/words'
 import './App.css'
 import Game from '../components/Game'
 import GameOver from '../components/GameOver'
@@ -15,13 +15,23 @@ function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
-  console.log(words);
+  const startGame = () => {
+    setGameStage(stages[1].name);
+  }
+
+  const verifyLetter = () => {
+    setGameStage(stages[2].name);
+  }
+
+  const retry = () => {
+    setGameStage(stages[0].name);
+  }
 
   return (
     <div className='App'>
-      {gameStage === stages[0].name && <StartScreen />}
-      {gameStage === stages[1].name && <Game/>}
-      {gameStage === stages[2].name && <GameOver/>}
+      {gameStage === stages[0].name && <StartScreen startGame={startGame} />}
+      {gameStage === stages[1].name && <Game verifyLetter={verifyLetter}/>}
+      {gameStage === stages[2].name && <GameOver retry={retry}/>}
     </div>
   )
 }
