f8283f53086420a480b59832f043b73fb61dc0ba
NedcloarBR <nedcloar1@hotmail.com>
2024-11-30 16:58:45 -0300

    feat: add aula 03 - handle form inputs

--- 5_FORMULARIOS/formularios/README.md
+++ 5_FORMULARIOS/formularios/README.md
@@ -12,3 +12,11 @@
   - Em React um padrão comum é a `tag label envolvendo o input`
   - Isso faz com que o atributo for se torne `opcional`
   - `Simplificando nossa estrutura de HTML`, sem perder a semântica
+
+# Aula 03
+
+- Manipulação de valores
+  - Para manipular os valores dos inputs vamos utilizar  o `hook useState`
+  - Ou seja, podemos armazenar na variável `e utilizar o set para alterar o valor`
+  - Vamos criar uma função para alterar o valor no evento `onChange`
+  - Deixando nosso código fácil de trabalhar nas próximas etapas: como envio dos dados para BD e validação
--- 5_FORMULARIOS/formularios/components/MyForm.jsx
+++ 5_FORMULARIOS/formularios/components/MyForm.jsx
@@ -1,13 +1,22 @@
+import { useState } from "react";
 import "./MyForm.css";
 
 const MyForm = () => {
+  // 3 - Gerenciamento de dados
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+
+  const handleName = (e) => {
+    setName(e.target.value);
+  }
+
   return (
     <div>
       {/* 1 - Criação de Form */}
       <form>
         <div>
           <label htmlFor="name">Nome:</label>
-          <input type="text" name="name" placeholder="Digite o seu nome" />
+          <input type="text" name="name" placeholder="Digite o seu nome" onChange={handleName} />
         </div>
         {/* 2 - label envolvendo input */}
         <label>
