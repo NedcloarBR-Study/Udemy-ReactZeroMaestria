57a1adf3c237561ef1b83b9092f62357ac9054c3
NedcloarBR <nedcloar1@hotmail.com>
2024-11-30 19:21:07 -0300

    feat: random choice category and word

--- 6_SECRET_WORD/secretword/src/App.jsx
+++ 6_SECRET_WORD/secretword/src/App.jsx
@@ -11,11 +11,31 @@ const stages = [
   { id: 3, name: "end"},
 ]
 
+const [pickedWord, setPickedWord] = useState("");
+const [pickedCategory, setPickedCategory] = useState("");
+const [pickedLetters, setPickedLetters] = useState([]);
+
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const pickWordAndCategory = () => {
+    const categories = Object.keys(words);
+    const category = categories[Math.floor(Math.random() * categories.length)];
+    const word = words[category][Math.floor(Math.random() * words[category].length)]
+
+    return { word, category };
+  }
+
   const startGame = () => {
+    const { word, category } = pickWordAndCategory();
+
+    const wordLetters = word.split("").map((letter) => letter.toLowerCase());
+
+    setPickedWord(word);
+    setPickedCategory(category);
+    setPickedLetters(wordLetters);
+
     setGameStage(stages[1].name);
   }
 
