72ce5a735af19e2c805a688d154fc56067e2bf1d
NedcloarBR <nedcloar1@hotmail.com>
2024-11-30 19:27:01 -0300

    feat: update game screen

--- 6_SECRET_WORD/secretword/src/App.jsx
+++ 6_SECRET_WORD/secretword/src/App.jsx
@@ -1,31 +1,32 @@
-import StartScreen from './components/StartScreen'
-import { useCallback, useEffect, useState } from 'react'
-import { wordsList } from './data/words'
-import './App.css'
-import Game from './components/Game'
-import GameOver from './components/GameOver'
+import StartScreen from "./components/StartScreen";
+import { useCallback, useEffect, useState } from "react";
+import { wordsList } from "./data/words";
+import "./App.css";
+import Game from "./components/Game";
+import GameOver from "./components/GameOver";
 
 const stages = [
-  { id: 1, name: "start"},
-  { id: 2, name: "game"},
-  { id: 3, name: "end"},
-]
-
-const [pickedWord, setPickedWord] = useState("");
-const [pickedCategory, setPickedCategory] = useState("");
-const [pickedLetters, setPickedLetters] = useState([]);
+  { id: 1, name: "start" },
+  { id: 2, name: "game" },
+  { id: 3, name: "end" },
+];
 
 function App() {
   const [gameStage, setGameStage] = useState(stages[0].name);
   const [words] = useState(wordsList);
 
+  const [pickedWord, setPickedWord] = useState("");
+  const [pickedCategory, setPickedCategory] = useState("");
+  const [pickedLetters, setPickedLetters] = useState([]);
+
   const pickWordAndCategory = () => {
     const categories = Object.keys(words);
     const category = categories[Math.floor(Math.random() * categories.length)];
-    const word = words[category][Math.floor(Math.random() * words[category].length)]
+    const word =
+      words[category][Math.floor(Math.random() * words[category].length)];
 
     return { word, category };
-  }
+  };
 
   const startGame = () => {
     const { word, category } = pickWordAndCategory();
@@ -37,23 +38,23 @@ function App() {
     setPickedLetters(wordLetters);
 
     setGameStage(stages[1].name);
-  }
+  };
 
   const verifyLetter = () => {
     setGameStage(stages[2].name);
-  }
+  };
 
   const retry = () => {
     setGameStage(stages[0].name);
-  }
+  };
 
   return (
-    <div className='App'>
+    <div className="App">
       {gameStage === stages[0].name && <StartScreen startGame={startGame} />}
-      {gameStage === stages[1].name && <Game verifyLetter={verifyLetter}/>}
-      {gameStage === stages[2].name && <GameOver retry={retry}/>}
+      {gameStage === stages[1].name && <Game verifyLetter={verifyLetter} />}
+      {gameStage === stages[2].name && <GameOver retry={retry} />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
