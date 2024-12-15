ad4fd5585e5f5ac639204f6252dc687c7905a558
NedcloarBR <nedcloar1@hotmail.com>
2024-12-15 10:22:07 -0300

    feat: add navbar and footer

---
+++ 10_MINI_BLOG/miniblog/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <h3>Escreva sobre o que você tem interesse!</h3>
+      <p>Mini Blog © 2024 </p>
+    </footer>
+  );
+};
+
+export default Footer;
