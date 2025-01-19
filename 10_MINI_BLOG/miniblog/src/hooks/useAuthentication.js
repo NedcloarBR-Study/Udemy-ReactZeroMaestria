66605425ffebcbcf8e204d2757d5c49384f7f882
NedcloarBR <nedcloar1@hotmail.com>
2025-01-18 23:20:54 -0300

    feat: add registration page with firebase

--- 10_MINI_BLOG/miniblog/src/hooks/useAuthentication.js
+++ 10_MINI_BLOG/miniblog/src/hooks/useAuthentication.js
@@ -1,3 +1,5 @@
+import { db } from "../firebase/config";
+
 import {
   getAuth,
   createUserWithEmailAndPassword,
@@ -10,7 +12,7 @@ import { useState, useEffect } from "react";
 
 export const useAuthentication = () => {
   const [error, setError] = useState("");
-  const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(false);
 
   // cleanup
   // deal with memory leak
@@ -23,4 +25,41 @@ export const useAuthentication = () => {
       return;
     }
   }
+
+  const createUser = async (data) => {
+    checkIfIsCancelled();
+    setError(null);
+    setLoading(true);
+
+    try {
+      const { user } = await createUserWithEmailAndPassword(
+        auth,
+        data.email,
+        data.password
+      );
+      await updateProfile(user, { displayName: data.username });
+      setLoading(false);
+      return user;
+    } catch (error) {
+      let systemErrorMessage = "";
+
+      if (error.message.includes("Password")) {
+        systemErrorMessage = "A senha precisa ter pelo menos 6 caracteres";
+      } else if (error.message.includes("email-already")) {
+        systemErrorMessage = "O email já está em uso";
+      } else {
+        systemErrorMessage = "Ocorreu um erro tente novamente mais tarde";
+      }
+      setLoading(false);
+      setError(systemErrorMessage);
+    }
+  };
+
+  useEffect(() => {
+    return () => {
+      setCancelled(true);
+    };
+  }, []);
+
+  return { auth, createUser, error, loading };
 };
