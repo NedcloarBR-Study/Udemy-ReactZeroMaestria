45511bce300f73afcd683cc88a37460adbf03ffe
NedcloarBR <nedcloar1@hotmail.com>
2025-01-23 15:27:28 -0300

    feat: add aula02 - useState and inputs

--- 11_HOOKS/hooks/README.md
+++ 11_HOOKS/hooks/README.md
@@ -5,3 +5,11 @@
   - O principal propósito é `gerenciar valores`
   - Poderemos `consultar` um valor e `alterar`
   - Isso nos permite `re-renderizar um componente`, o que não acontece com manipulação de variáveis
+
+# Aula 02
+
+- useState e inputs
+  - `Atrelando o useState a um input` podemos fazer algumas ações
+  - Alteração de um state por evento de `onChange`
+  - Limpeza de inputs (`Controlled Input`)
+  - Após preenchimento total do form, unir os states e fazer um envio dos dados para o back-end
--- 11_HOOKS/hooks/src/components/HookUseState.jsx
+++ 11_HOOKS/hooks/src/components/HookUseState.jsx
@@ -13,6 +13,13 @@ const HookUseState = () => {
 
   console.log(name);
 
+  // 2 - useState e input
+  const [age, setAge] = useState(18);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(age);
+  };
+
   return (
     <div>
       {/* 1 - useState */}
@@ -20,6 +27,18 @@ const HookUseState = () => {
       <p>Variável: {userName}</p>
       <p>useState: {name}</p>
       <button onClick={changeNames}>Mudar nomes!</button>
+      {/* 2 - useState e input */}
+      <p>Digite a sua idade: </p>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input type="submit" value="enviar" />
+      </form>
+      <p>Você tem {age} anos!</p>
+      <hr />
     </div>
   );
 };
