a0044b4ac58c5a259d380619d811413083da3109
NedcloarBR <nedcloar1@hotmail.com>
2025-01-26 17:13:01 -0300

    feat: add aula14 - useLayoutEffect

--- 11_HOOKS/hooks/src/pages/Home.jsx
+++ 11_HOOKS/hooks/src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import HookUseCallback from "../components/HookUseCallback";
 import { SomeContext } from "../components/HookUseContext";
 import HookUseEffect from "../components/HookUseEffect";
+import HookUseLayoutEffect from "../components/HookUseLayoutEffect";
 import HookUseMemo from "../components/HookUseMemo";
 import HookUseReducer from "../components/HookUseReducer";
 import HookUseRef from "../components/HookUseRef";
@@ -23,6 +24,7 @@ const Home = () => {
       <HookUseRef />
       <HookUseCallback />
       <HookUseMemo />
+      <HookUseLayoutEffect />
     </div>
   );
 };
